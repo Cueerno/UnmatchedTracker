c81449496a3bea950d3e9642ae45165c138d6fad
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-25 21:50:44 +0300

    initial commit: telegram bot

---
+++ frontend/unmatched-telegram-bot/src/main/java/com/radiuk/unmatched_telegram_bot/UnmatchedTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.radiuk.unmatched_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmatchedTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnmatchedTelegramBotApplication.class, args);
+    }
+
+}
---
+++ frontend/unmatched-telegram-bot/src/test/java/com/radiuk/unmatched_telegram_bot/UnmatchedTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radiuk.unmatched_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnmatchedTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
