e1440566bd0239acd98790afec80c62c5e48a20e
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-25 21:49:24 +0300

    initial commit: backend core service

---
+++ backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/UnmatchedBackendCoreApplication.java
@@ -0,0 +1,13 @@
+package com.radiuk.unmatched_backend_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmatchedBackendCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnmatchedBackendCoreApplication.class, args);
+	}
+
+}
