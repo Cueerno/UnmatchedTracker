fc1146a1c69037d69a3c588f827ebe9ce5808cd3
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-07-30 19:12:52 +0300

    feat: add read methods for characters
    - get all characters
    - get character by name

--- backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/controller/CharacterController.java
+++ backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/controller/CharacterController.java
@@ -4,6 +4,7 @@ import com.radiuk.unmatched_backend_core.service.CharacterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,18 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
-    @GetMapping("top")
+    @GetMapping("/all")
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.ok(characterService.getAll());
+    }
+
+    @GetMapping("/top")
     public ResponseEntity<?> getFavoriteCharacters() {
         return ResponseEntity.ok(characterService.getRating());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<?> getByName(@PathVariable String name) {
+        return ResponseEntity.ok(characterService.getByName(name));
     }
 }
--- backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/service/CharacterService.java
+++ backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/service/CharacterService.java
@@ -1,7 +1,10 @@
 package com.radiuk.unmatched_backend_core.service;
 
+import com.radiuk.unmatched_backend_core.dto.CharacterDto;
 import com.radiuk.unmatched_backend_core.dto.CharacterRatingDto;
+import com.radiuk.unmatched_backend_core.mapper.CharacterMapper;
 import com.radiuk.unmatched_backend_core.repository.CharacterRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +16,20 @@ import java.util.List;
 public class CharacterService {
 
     private final CharacterRepository characterRepository;
+    private final CharacterMapper characterMapper;
+
+    @Transactional(readOnly = true)
+    public List<CharacterDto> getAll() {
+        return characterMapper.toDtos(characterRepository.findAll());
+    }
 
     @Transactional(readOnly = true)
     public List<CharacterRatingDto> getRating() {
         return characterRepository.getRating();
+    }
+
+    @Transactional(readOnly = true)
+    public CharacterDto getByName(String name) {
+        return characterMapper.toDto(characterRepository.findByName(name).orElseThrow(EntityNotFoundException::new));
     }
 }
