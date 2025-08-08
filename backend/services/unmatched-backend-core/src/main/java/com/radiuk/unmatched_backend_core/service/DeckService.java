a5d2120f6f2b1e5904fd44c3ea007dc3d4eda9f5
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-08-08 22:38:35 +0300

    feat: change deck service for caching

--- backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/service/DeckService.java
+++ backend/services/unmatched-backend-core/src/main/java/com/radiuk/unmatched_backend_core/service/DeckService.java
@@ -2,36 +2,31 @@ package com.radiuk.unmatched_backend_core.service;
 
 import com.radiuk.unmatched_backend_core.dto.DeckDto;
 import com.radiuk.unmatched_backend_core.dto.DeckRatingDto;
-import com.radiuk.unmatched_backend_core.mapper.DeckMapper;
-import com.radiuk.unmatched_backend_core.repository.DeckRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.radiuk.unmatched_backend_core.model.Match;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static com.radiuk.unmatched_backend_core.util.SortUtil.getSort;
-
 @Service
 @RequiredArgsConstructor
 public class DeckService {
 
-    private final DeckRepository deckRepository;
-    private final DeckMapper deckMapper;
+    private final DeckCacheService deckCacheService;
 
     @Transactional(readOnly = true)
     public List<DeckDto> getAll(String sortBy, String direction) {
-        return deckMapper.toDtos(deckRepository.findAll(getSort(sortBy, direction)));
-    }
-
-    @Transactional(readOnly = true)
-    public List<DeckRatingDto> getTopByWins() {
-        return deckRepository.getTopByWins();
+        return deckCacheService.getAllFromCache(sortBy, direction);
     }
 
     @Transactional(readOnly = true)
     public DeckDto getByName(String name) {
-        return deckMapper.toDto(deckRepository.findByName(name).orElseThrow(() -> new EntityNotFoundException("Deck with name" + name + "not found")));
+        return deckCacheService.getFromCache(name);
+    }
+
+    @Transactional(readOnly = true)
+    public List<DeckRatingDto> getTop(Match.MatchFormat format) {
+        return deckCacheService.getTopFromCache(format);
     }
 }
