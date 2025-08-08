7c56e908270bc7cf670134ec47863b91c2e4a39b
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-08-08 22:35:32 +0300

    feat: combine sets, decks and daughters into one list in the header

--- frontend/unmatched-frontend/src/components/Header_and_Footer/Header.tsx
+++ frontend/unmatched-frontend/src/components/Header_and_Footer/Header.tsx
@@ -1,8 +1,35 @@
+import React, {useEffect, useRef, useState} from 'react';
 import {Link} from 'react-router-dom';
-import {FaBoxOpen, FaChessBoard, FaHome, FaLayerGroup, FaPlus, FaUser, FaUsers} from 'react-icons/fa';
+import {
+    FaBoxOpen,
+    FaCaretDown,
+    FaChartBar,
+    FaChessBoard,
+    FaHome,
+    FaLayerGroup,
+    FaPlus,
+    FaUser,
+    FaUsers
+} from 'react-icons/fa';
 import './Header.css';
 
 export default function Header() {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(e.target as Node)
+            ) {
+                setDropdownOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, []);
+
     return (
         <header className="header">
             <h2>Unmatched Tracker</h2>
@@ -24,31 +51,58 @@ export default function Header() {
 
                 <Link to="/parties" className="nav-link">
                     <span className="nav-link-inner">
-                     <FaUsers/>
-                         <span>Parties</span>
-                     </span>
+                        <FaUsers/>
+                        <span>Parties</span>
+                    </span>
                 </Link>
 
-                <Link to="/sets" className="nav-link">
+                <div
+                    className="nav-link dropdown"
+                    ref={dropdownRef}
+                    onClick={() => setDropdownOpen(open => !open)}
+                >
                     <span className="nav-link-inner">
-                     <FaBoxOpen/>
-                         <span>Sets</span>
-                     </span>
+                        <span>UmDb</span>
+                        <FaCaretDown/>
+                    </span>
+
+                    {dropdownOpen && (
+                        <ul className="dropdown-menu">
+                            <li>
+                                <Link to="/sets" className="dropdown-item">
+                                    <span className="nav-link-inner">
+                                        <FaBoxOpen/>
+                                        <span>Sets</span>
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/decks" className="dropdown-item">
+                                    <span className="nav-link-inner">
+                                        <FaLayerGroup/>
+                                        <span>Decks</span>
+                                    </span>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/boards" className="dropdown-item">
+                                    <span className="nav-link-inner">
+                                        <FaChessBoard/>
+                                        <span>Boards</span>
+                                    </span>
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                </div>
+
+                <Link to="/decks/top" className="nav-link">
+                    <span className="nav-link-inner">
+                        <FaChartBar/>
+                        <span>Top</span>
+                    </span>
                 </Link>
 
-                <Link to="/decks" className="nav-link">
-                    <span className="nav-link-inner">
-                        <FaLayerGroup/>
-                         <span>Decks</span>
-                     </span>
-                </Link>
-
-                <Link to="/boards" className="nav-link">
-                    <span className="nav-link-inner">
-                     <FaChessBoard/>
-                         <span>Boards</span>
-                     </span>
-                </Link>
             </nav>
         </header>
     );
