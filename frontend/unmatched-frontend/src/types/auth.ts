255e57aea187fc08b64776072e58ed5a7fd4c18f
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-10-06 19:02:25 +0300

    feat (frontend): add dtos

---
+++ frontend/unmatched-frontend/src/types/auth.ts
@@ -0,0 +1,12 @@
+export interface RegistrationDto {
+
+    username: string;
+    password: string;
+    email: string;
+}
+
+export interface AuthDto {
+
+    username: string;
+    password: string;
+}
