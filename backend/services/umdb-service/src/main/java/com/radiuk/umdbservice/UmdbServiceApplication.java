8ed17a54065ecf9958449f9b5eea17efd0ae4354
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-10-11 23:55:55 +0300

    initial commit: umdb-service

---
+++ backend/services/umdb-service/src/main/java/com/radiuk/umdbservice/UmdbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.radiuk.umdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmdbServiceApplication.class, args);
+	}
+
+}
