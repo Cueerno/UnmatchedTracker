6dcb3839fc31c4df2ed2c6b9d73674a755bf5720
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-10-16 20:40:19 +0300

    fix: add redis caching

--- backend/services/umdb-service/src/main/java/com/radiuk/umdb_service/UmdbServiceApplication.java
+++ backend/services/umdb-service/src/main/java/com/radiuk/umdb_service/UmdbServiceApplication.java
@@ -2,7 +2,9 @@ package com.radiuk.umdb_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class UmdbServiceApplication {
 
---
+++ backend/services/umdb-service/src/main/java/com/radiuk/umdb_service/config/RedisCacheConfig.java
@@ -0,0 +1,98 @@
+package com.radiuk.umdb_service.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.radiuk.umdb_service.dto.BoardDto;
+import com.radiuk.umdb_service.dto.CardWithDecksDto;
+import com.radiuk.umdb_service.dto.DeckWithCardsDto;
+import com.radiuk.umdb_service.dto.SetDto;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .build();
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory, ObjectMapper objectMapper) {
+        RedisCacheConfiguration baseConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new StringRedisSerializer())
+                );
+
+        Function<CacheConfigEntry, RedisCacheConfiguration> createConfig =
+                entry -> baseConfig
+                        .entryTtl(entry.ttl)
+                        .serializeValuesWith(
+                                RedisSerializationContext
+                                        .SerializationPair
+                                        .fromSerializer(entry.valueSerializer)
+                        );
+
+        Jackson2JsonRedisSerializer<SetDto> setSerializer = new Jackson2JsonRedisSerializer<>(SetDto.class);
+        setSerializer.setObjectMapper(objectMapper);
+
+        Jackson2JsonRedisSerializer<DeckWithCardsDto> deckSerializer = new Jackson2JsonRedisSerializer<>(DeckWithCardsDto.class);
+        deckSerializer.setObjectMapper(objectMapper);
+
+        Jackson2JsonRedisSerializer<CardWithDecksDto> cardSerializer = new Jackson2JsonRedisSerializer<>(CardWithDecksDto.class);
+        cardSerializer.setObjectMapper(objectMapper);
+
+        Jackson2JsonRedisSerializer<BoardDto> boardSerializer = new Jackson2JsonRedisSerializer<>(BoardDto.class);
+        boardSerializer.setObjectMapper(objectMapper);
+
+        GenericJackson2JsonRedisSerializer listSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        Map<String, RedisCacheConfiguration> configs = new HashMap<>();
+
+        configs.put("setList",       createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), listSerializer)));
+        configs.put("set",           createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), setSerializer)));
+
+        configs.put("deckList",      createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), listSerializer)));
+        configs.put("deck",          createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), deckSerializer)));
+        configs.put("deckTop",       createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), listSerializer)));
+
+        configs.put("cardList",      createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), listSerializer)));
+        configs.put("card",          createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), cardSerializer)));
+
+        configs.put("boardList",     createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), listSerializer)));
+        configs.put("board",         createConfig.apply(new CacheConfigEntry(Duration.ofMinutes(10), boardSerializer)));
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(baseConfig)
+                .withInitialCacheConfigurations(configs)
+                .transactionAware()
+                .build();
+    }
+
+    private record CacheConfigEntry(
+            Duration ttl,
+            RedisSerializer<?> valueSerializer
+    ) {}
+}
