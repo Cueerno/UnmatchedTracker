ae07dfedb1b3bb8ff9d39d8769a9819bbe31aa02
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-10-21 23:37:42 +0300

    feat: add application.properties and change port on frontend

--- frontend/unmatched-frontend/src/api/party.ts
+++ frontend/unmatched-frontend/src/api/party.ts
@@ -1,7 +1,7 @@
 import {PartyPayload} from "../types/party";
-import {API_BASE_URL} from "./config";
+import {PARTY_API_BASE_URL} from "./config";
 
-const backUrl = `${API_BASE_URL}/parties`;
+const backUrl = `${PARTY_API_BASE_URL}/parties`;
 
 export async function getPartyByMatchId(matchId: number) {
     const res = await fetch(`${backUrl}/${matchId}`, {
