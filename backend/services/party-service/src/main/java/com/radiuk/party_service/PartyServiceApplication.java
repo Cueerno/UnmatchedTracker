2f28c843c6cf20443be9e3ffb0688fa94c342c12
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-10-25 00:35:10 +0300

    feat: add @EnableFeignClients to main class

--- backend/services/party-service/src/main/java/com/radiuk/party_service/PartyServiceApplication.java
+++ backend/services/party-service/src/main/java/com/radiuk/party_service/PartyServiceApplication.java
@@ -3,10 +3,12 @@ package com.radiuk.party_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableCaching
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class PartyServiceApplication {
 
