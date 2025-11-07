1899d3911cdccdaa0c02b4026f33452c7d955865
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-11-07 23:18:29 +0300

    feat: add logging and add aspects - LoggingAspect and ExceptionLoggingAspect

---
+++ backend/services/backup-service/src/main/java/com/radiuk/backup_service/aspect/ExceptionLoggingAspect.java
@@ -0,0 +1,23 @@
+package com.radiuk.backup_service.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ExceptionLoggingAspect {
+
+    @AfterThrowing(
+            pointcut = "within(@org.springframework.web.bind.annotation.RestControllerAdvice *)",
+            throwing = "exception"
+    )
+    public void logException(JoinPoint joinPoint, Throwable exception) {
+        String methodName = joinPoint.getSignature().toShortString();
+        log.error("[ExceptionHandler] -> {} handled exception: {}",
+                methodName, exception.getMessage(), exception);
+    }
+}
---
+++ backend/services/backup-service/src/main/java/com/radiuk/backup_service/aspect/LoggingAspect.java
@@ -0,0 +1,44 @@
+package com.radiuk.backup_service.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(public * com.radiuk.backup_service.service..*(..))")
+    public void serviceMethods() {}
+
+    @Before("serviceMethods()")
+    public void logMethodCall(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        log.debug("[{}.{}] -> called with args={}", className, methodName, args);
+    }
+
+    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
+    public void logMethodReturn(JoinPoint joinPoint, Object result) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        if (result instanceof List<?>) {
+            log.info("[{}.{}] -> finished successfully: returned list with size={}", className, methodName, ((List<?>) result).size());
+        } else {
+            log.info("[{}.{}] -> finished successfully: result={}", className, methodName, result);
+        }
+    }
+
+    @AfterThrowing(pointcut = "serviceMethods()", throwing = "ex")
+    public void logMethodException(JoinPoint joinPoint, Exception ex) {
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        log.warn("[{}.{}] -> exception thrown: {}", className, methodName, ex.getMessage(), ex);
+    }
+}
